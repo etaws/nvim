f932041e9cc9ff29ca9b6653a95a22c1fa5bb78b
etaws <etawsz@gmail.com>
2022-05-02 12:54:45 +0800

    remove warning on lua

--- lua/plugin-config/lsp.lua
+++ lua/plugin-config/lsp.lua
@@ -31,6 +31,11 @@ for _, lsp in pairs(servers) do
     require('lspconfig')[lsp].setup {
         on_attach = on_attach,
         capabilities = capabilities,
+        settings = {
+            Lua = {
+                diagnostics = { globals = { 'vim' } }
+            }
+        },
         flags = {
             -- This will be the default in neovim 0.7+
             debounce_text_changes = 150,
